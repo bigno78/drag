98c0fcb85ddc44c85e87c73420916405752436f7
Miroslav Demek <xdemek@fi.muni.cz>
2020-02-25 17:23:31 +0100

    plserino pullerino

--- src/dot.cpp
+++ src/dot.cpp
@@ -16,19 +16,25 @@ struct derived : base<derived> {
 
 int main() {
     graph g = graph_builder()
-                .add_edge(0, 2).add_edge(0, 4)
-                .add_edge(1, 2).add_edge(1, 4).add_edge(1, 3)
+                .add_edge(0, 11).add_edge(0, 6).add_edge(0, 4)
+                .add_edge(1, 2).add_edge(1, 5)
                 .add_edge(2, 5)
-                .add_edge(3, 4).add_edge(3, 5).add_edge(3, 6)
-                .add_edge(4, 6)
-                .add_edge(5, 6)
+                .add_edge(3, 8).add_edge(3, 6)
+                .add_edge(4, 9).add_edge(4, 10).add_edge(4, 7)
+                .add_edge(5, 8)
+                .add_edge(6, 9)
+                .add_edge(7, 10)
+                .add_edge(11, 1)
                 .build();
 
     auto ranking = initialize_ranking(g);
-
+    
     for (vertex_t u = 0; u < g.size(); ++u) {
         std::cout << u << ": " << ranking[u] << "\n";
     }
+    std::cout << "\n";
 
     auto tree = initialize_tree(g, ranking);
+    std::cout << "\n";
+    std::cout << tree;
 }
