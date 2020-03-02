7c58c2bbd486d1b292d3a9aa5591000f2cc6e3b1
Miroslav Demek <xdemek@fi.muni.cz>
2020-03-02 11:11:47 +0100

    splitting graph into components

--- src/layout.hpp
+++ src/layout.hpp
@@ -13,6 +13,10 @@ class sugiyama_layout {
 
     std::unique_ptr< detail::cycle_removal > cycle_rem;
 
+    std::vector< std::vector<vertex_t> > edges;
+    std::vector< std::vector< std::vector<vertex_t> > > long_edges;
+
+
 public:
     sugiyama_layout(graph& g) : g(g) {}
 
@@ -22,5 +26,36 @@ public:
     void layer_separation(float d);
 
 private:
-    void split();
+    void split() {
+        std::vector< std::vector<vertex_t> > components;
+        std::vector< bool > done(g.size(), false);
+        for (auto u : g.vertices()) {
+            if (!done[u]) {
+                components.emplace_back();
+                split(done, components.back(), u);
+            }
+        }
+
+        for (auto component : components) {
+            subgraphs.emplace_back(g, component);
+        }
+    }
+
+    void split(std::vector<bool>& done, std::vector<vertex_t>& component, vertex_t u) {
+        component.push_back(u);
+        for (auto v : g.out_neighbours(u)) {
+            if (!done[v]) {
+                split(done, component, v);
+            }
+        }
+        for (auto v : g.in_neighbours(u)) {
+            if (!done[v]) {
+                split(done, component, v);
+            }
+        }
+    }
+
+    void add_dummy_nodes(detail::subgraph& g, std::vector< std::vector<vertex_t> > hierarchy) {
+        
+    }
 };
