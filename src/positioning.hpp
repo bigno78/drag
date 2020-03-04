9e5b8375fdb866e9d92dd324d523352eaa43fd31
Miroslav Demek <xdemek@fi.muni.cz>
2020-03-04 20:07:53 +0100

    positioning attributes

--- src/positioning.hpp
+++ src/positioning.hpp
@@ -8,13 +8,8 @@
 namespace detail {
 
 struct positioning_attributes {
-    const std::vector<float>& sizes;
     float node_dist;
     float layer_dist;
-
-    float min_dist(vertex_t u, vertex_t v) {
-        return sizes[u] + sizes[v] + node_dist;
-    }
 };
 
 struct positioning {
@@ -24,16 +19,20 @@ struct positioning {
 
 struct test_positioning : public positioning {
     std::vector<vec2>& positions;
-    positioning_attributes attr;  
+    positioning_attributes attr;
+
+    test_positioning(std::vector<vec2>& positions, positioning_attributes attr)
+        : positions(positions)
+        , attr(std::move(attr)) {}  
 
     void run(const subgraph& g, const hierarchy& h) override {
         float y = attr.layer_dist;
         for (auto layer : h.layers) {
             float x = attr.node_dist;
             for (auto u : layer) {
-                x += attr.sizes[u];
+                x += g.node_size(u);
                 positions[u] = { x, y };
-                x += attr.sizes[u] + attr.node_dist;
+                x += g.node_size(u) + attr.node_dist;
             }
             y += attr.layer_dist;
         }
