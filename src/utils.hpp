d12f7b6085385fba86c9be253cf147b84cf1663b
Miroslav Demek <xdemek@fi.muni.cz>
2020-04-09 15:12:57 +0200

    add default value for step, fix vertex_t return type

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,16 +1,14 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "vec2.hpp"
 #include "types.hpp"
 
 
 template <typename T> 
-int sgn(T val) {
-    return (T(0) < val) - (val < T(0));
-}
-
+int sgn(T val) { return ( T(0) < val ) - ( val < T(0) ); }
 
 
 template<typename T>
@@ -19,7 +17,7 @@ struct range {
     T r_end; 
     int step;
 
-    range(T start, T end, int step) : r_start(start), r_end(end), step(step) {}
+    range(T start, T end, int step = 1) : r_start(start), r_end(end), step(step) {}
 
     struct iterator {
         T val;
@@ -35,7 +33,6 @@ struct range {
     iterator begin() const { return iterator(r_start, step); }
     iterator end() const { return iterator(r_end, step); }
 };
-
 
 
 template<typename T>
@@ -61,7 +58,7 @@ struct chain_range {
             }    
         }
 
-        vertex_t operator*() { return *curr; }
+        typename T::value_type operator*() { return *curr; }
         friend bool operator==(const iterator& lhs, const iterator& rhs) { return lhs.curr == rhs.curr; }
         friend bool operator!=(const iterator& lhs, const iterator& rhs) { return !(lhs == rhs); }
         iterator& operator++() {
@@ -72,10 +69,18 @@ struct chain_range {
         }
     };
 
-    iterator begin() { return iterator(std::begin(first), std::end(first), std::begin(second)); }
-    iterator begin() const { return iterator(std::begin(first), std::end(first), std::begin(second)); }
-    iterator end() { return iterator(std::end(second), std::end(first), std::begin(second)); }
-    iterator end() const { return iterator(std::end(second), std::end(first), std::begin(second)); }
+    iterator begin() {
+        return iterator( std::begin(first), std::end(first), std::begin(second) );
+    }
+    iterator begin() const {
+        return iterator( std::begin(first), std::end(first), std::begin(second) );
+    }
+    iterator end() {
+        return iterator( std::end(second),  std::end(first), std::begin(second) );
+    }
+    iterator end() const {
+        return iterator( std::end(second),  std::end(first), std::begin(second) );
+    }
 };
 
 
