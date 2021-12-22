205630a82bfa07df2d38afe2ffd67a3b2fdd3b40
Miroslav Demek <xdemek@fi.muni.cz>
2021-12-22 10:43:24 +0100

    Do some things in this soon to be removed file

--- test/my-main.cpp
+++ test/my-main.cpp
@@ -2,11 +2,11 @@
 #include <drag/drawing/svg.hpp>
 #include <drag/drawing/draw.hpp>
 
-#include "bruteforce.hpp"
+#include "utils/bruteforce.hpp"
 #include "test-utils.hpp"
 #include "../example/helper.hpp"
 
-#include "../generator/gen.hpp"
+#include "utils/gen.hpp"
 
 using namespace drag;
 using namespace drag::detail;
@@ -22,14 +22,16 @@ int main() {
                 .add_edge(3, 11).add_edge(11, 5).add_edge(0, 12)
                 .add_edge(12, 13).add_edge(13, 9)
                 .build();
+    
     subgraph sub = make_subgraph(g);
     drawing_options opts;
     dag_generator gen;
     network_simplex_layering layering_module;
 
-    for (int i = 0; i < 20; ++i) {
-        //auto g = gen.generate_from_density(15, 0.2);
-        auto g = gen.generate_from_edges(15, 20);
+    std::vector<uint64_t> times;
+
+    for (int i = 0; i < 100; ++i) {
+        auto g = gen.generate_from_edges(17, 20);
         auto sub = make_subgraph(g);
 
         auto h = layering_module.run(sub);
@@ -40,15 +42,25 @@ int main() {
         auto start = now();
         size_t brute_len = bruteforce_layering_total_length(g);
         auto end = now();
+        auto dur = to_micro(end - start);
 
-        std::cout << "Simplex: " << simplex_len << "\n";
-        std::cout << "Brute:   " << brute_len << "\n";
-        std::cout << "time: " << to_mili(end - start) << "\n\n";
+        times.push_back(dur);
+
+        //std::cout << "Simplex: " << simplex_len << "\n";
+        //std::cout << "Brute:   " << brute_len << "\n";
+        //std::cout << "time: " << to_mili(end - start) << "\n\n";
 
         if (simplex_len != brute_len) {
             std::cout << "FAIL!!!!\n";
+            return 1;
         }
         
-        draw_svg_file("stuff/" + std::to_string(i) + ".svg", g, {});
+        //draw_svg_file("stuff/" + std::to_string(i) + ".svg", g, {});
     }
+
+    float avg = std::accumulate(times.begin(), times.end(), uint64_t(0))/float(times.size());
+    uint64_t max = *std::max_element(times.begin(), times.end());
+
+    std::cout << "AVG: " << avg << "\n";
+    std::cout << "MAX: " << max << "\n";
 }
