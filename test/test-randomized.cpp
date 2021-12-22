cb15225048ccb070760d44dbaf6c67e1cf04a356
Miroslav Demek <xdemek@fi.muni.cz>
2021-12-22 10:45:47 +0100

    Get the random tests working

--- test/test-randomized.cpp
+++ test/test-randomized.cpp
@@ -1,37 +1,16 @@
-#include <drag/drawing/draw.hpp>
-
-#include "utils/gen.hpp"
-#include "utils/bruteforce.hpp"
-
-#include <iostream>
-
-template<typename Func>
-bool run_randomized_test(Func test_func,
-                         std::string test_name,
-                         size_t n,
-                         size_t m,
-                         size_t count = 20,
-                         size_t seed = 7)
-{
-    drag::dag_generator gen(seed);
-
-    for (size_t i = 0; i < count; ++i) {
-        drag::graph g = gen.generate_from_edges(n, m);
-        
-        if (!test_func(g)) {
-            std::string filename = "error.svg";
-
-            std::cerr << "TEST FAILED: " << test_name << "\n";
-            std::cerr << "Drawing the graph to: '" << filename << "'\n";
-            
-            drag::draw_svg_file(filename, g);
-            return false;
-        }    
-    }
-
-    return true;
-}
+#include "utils/randomized.hpp"
 
 int main() {
 
+    Tester tester(7);
+
+    tester.register_test("thingy", [] (const drag::graph& g) {
+        return true;
+    });
+
+    tester.register_test("mingy", [] (const drag::graph& g) {
+        return false;
+    });
+
+    return tester.run_tests();
 }
